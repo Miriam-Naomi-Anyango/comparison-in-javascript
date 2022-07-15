d9ad9c35f7d564d45bf7d03a25e683e993f729e7
Miriam Anyango <minaoanya@gmail.com>
2022-07-15 08:56:18 +0300

    brief recap on comparison in javascript

---
+++ index.js
@@ -0,0 +1,27 @@
+// comparison operators enable us to check to see if a value is what we're expecting.
+// JavaScript includes both strict and loose comparison operators. 
+// When writing JavaScript, you should strongly prefer the strict operators.
+// The reason for this is the loose operators will return true even if the data types aren't the same,
+//  which can result in unexpected behavior and bugs that can be difficult to track down.
+
+/*JavaScript includes four equality operators:
+
+strict equality operator (===)
+strict inequality operator (!==)
+loose equality operator (==)
+loose inequality operator (!=)
+These operators allow us to compare values and determine whether they are the same.*/
+
+
+/*Compare Numbers with Relational Operators
+JavaScript includes four relational operators:
+
+greater than (>)
+greater than or equals (>=)
+less than (<)
+less than or equals (<=) 
+
+The behavior of these operators is consistent with the meaning of 
+the corresponding symbols in mathematics:*/
+
+// Stick to comparing numerical values with the relational operators and you'll be golden
